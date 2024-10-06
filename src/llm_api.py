3b3b76ad7ca39e7becaac6a7db099fd5f97058bf
Denis Chilik <deniskocs@gmail.com>
2024-10-06 10:54:48 -0400

    fix linting

--- src/llm_api.py
+++ src/llm_api.py
@@ -1,4 +1,6 @@
-from flask import jsonify, request
+from typing import Dict
+
+from flask import jsonify, request, Response
 from flask.views import MethodView
 
 from model_holder import ModelHolder
@@ -9,13 +11,15 @@ class LLMAPI(MethodView):
     def __init__(self, model_holder: ModelHolder):
         self.model_holder = model_holder
 
-    def options(self):
+    def options(self) -> tuple[str, int]:
         # You can include logic for handling OPTIONS requests here if needed
         return '', 204
 
-    def post(self):
+    def post(self) -> tuple[Response, int]:
         json_data = request.json
 
-        output = self.model_holder.generate_text(json_data["history"])
+        if isinstance(json_data, Dict):
+            output = self.model_holder.generate_text(json_data["history"])
+            return jsonify({"output": output}), 200
 
-        return jsonify({"output": output}), 200
+        return jsonify({}), 400
--- src/model_holder.py
+++ src/model_holder.py
@@ -1,26 +1,35 @@
 import time
+from typing import Dict, List
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
+# pylint: disable=too-few-public-methods
 class ModelHolder:
-    def __init__(self):
-        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3", device_map="auto",
-                                                          torch_dtype=torch.bfloat16)
-        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+    device: torch.device
 
-    def generate_text(self, history) -> str:
+    def __init__(self, model: str = "mistralai/Mistral-7B-Instruct-v0.3") -> None:
+        self.device = torch.device(
+            "cuda:0" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model,
+            torch_dtype=torch.bfloat16
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
+        self.model.to(self.device)
+
+    def generate_text(self, history: List[Dict[str, str]]) -> str:
         print("\n--------------------------\n")
         for element in history:
             print(f"role: {element['role']}\n")
             print(f"content:\n{element['content']}\n")
 
-        encodeds = self.tokenizer.apply_chat_template(history, return_tensors="pt")
-        print("\n--------------------------\n")
-        print(f"Number of received tokens: {encodeds.size()[1]}")
+        model_inputs = self.tokenizer.apply_chat_template(history, return_tensors="pt").to(self.device)
 
-        model_inputs = encodeds.to('mps')
+        print("\n--------------------------\n")
+        print(f"Number of received tokens: {model_inputs.size()[1]}")
 
         start_time = time.time()
         generated_ids = self.model.generate(
@@ -32,11 +41,11 @@ class ModelHolder:
         )
         end_time = time.time()
 
-        input = self.tokenizer.decode(model_inputs[0], skip_special_tokens=True)
-        prompt_len = len(input)
+        decoded_input = self.tokenizer.decode(model_inputs[0], skip_special_tokens=True)
+        prompt_len = len(decoded_input)
         output_tokens_len = len(generated_ids[0])
-        decoded = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-        output = decoded[prompt_len + 1:]
+        decoded: str = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        output: str = decoded[prompt_len + 1:]
 
         generation_time = end_time - start_time
         tokens_per_second = output_tokens_len / generation_time
@@ -46,4 +55,3 @@ class ModelHolder:
         print(output)
         print("\n\n\n\n\n")
         return output
-
